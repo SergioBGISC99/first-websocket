3880e4bf92c78f48c17d4ee29f43ad7470d6640d
Sergio Barreras <sergiobg.isc@gmail.com>
2024-01-23 19:08:15 -0700

    escuchar mensajes del servidor

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,24 @@
-console.log('Hola mundo');
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 3000 });
+
+wss.on("connection", function connection(ws) {
+  console.log("Client connected");
+
+  ws.on("error", console.error);
+
+  ws.on("message", function message(data) {
+    console.log("received: %s", data);
+    const payload = {
+      type: "custom-message",
+      payload: data.toString(),
+    };
+    ws.send(JSON.stringify(payload));
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
+console.log("http://localhost:3000");
