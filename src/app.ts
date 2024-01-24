1723e5cd0c06cc65c81f7199a6c6a200352c0b41
Sergio Barreras <sergiobg.isc@gmail.com>
2024-01-23 21:36:14 -0700

    comunicación excluyente

--- src/app.ts
+++ src/app.ts
@@ -12,10 +12,17 @@ wss.on("connection", function connection(ws) {
       type: "custom-message",
       payload: data.toString(),
     });
-    //ws.send(JSON.stringify(payload));
 
+    //* TODOS INCLUYENTE
+    // wss.clients.forEach(function each(client) {
+    //   if (client.readyState === WebSocket.OPEN) {
+    //     client.send(payload, { binary: false });
+    //   }
+    // });
+
+    //* TODOS EXCLUYENTE
     wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(payload, { binary: false });
       }
     });
